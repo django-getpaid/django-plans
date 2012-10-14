0fc43edd3c73453056fc2e5b606a67deaeba1699
Krzysztof Dorosz <cypreess@gmail.com>
2012-10-15 01:19:10 +0200

    Drobne poprawki zwiazane z uzyciem get_user_quota

--- plans/quota.py
+++ plans/quota.py
@@ -3,5 +3,5 @@ from plans.models import PlanQuota
 def get_user_quota(user):
     quota_dic = {}
     for plan_quota in PlanQuota.objects.filter(plan__userplan__user=user).select_related('quota'):
-        quota_dic[plan_quota.quota.codename] = (plan_quota.quota, plan_quota.value)
+        quota_dic[plan_quota.quota.codename] = plan_quota.value
     return quota_dic
