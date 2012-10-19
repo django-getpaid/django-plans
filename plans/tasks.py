8703258a4e34267b7080e2ba55a1aa79a79edf96
Krzysztof Dorosz <cypreess@gmail.com>
2012-10-19 18:08:35 +0200

    account expiration is not managed by celery periodic task which fire it up every night after midnight.

---
+++ plans/tasks.py
@@ -0,0 +1,24 @@
+import datetime
+import logging
+from celery.schedules import crontab
+from celery.task.base import periodic_task
+from django.conf import settings
+from django.contrib.auth.models import User
+
+logger = logging.getLogger('plans.tasks')
+
+@periodic_task(run_every=crontab(hour=0, minute=5))
+def expire_account():
+
+    logger.info('Started')
+
+    for user in User.objects.select_related('userplan').filter(userplan__active=True,
+        userplan__expire__lt=datetime.date.today()):
+        user.userplan.expire_account()
+
+    notifications_days_before = getattr(settings, 'PLAN_EXPIRATION_REMIND', [])
+
+    if notifications_days_before:
+        days = map(lambda x: datetime.date.today() + datetime.timedelta(days=x), notifications_days_before)
+        for user in User.objects.select_related('userplan').filter(userplan__active=True, userplan__expire__in=days):
+            user.userplan.remind_expire_soon()
