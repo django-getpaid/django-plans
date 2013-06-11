07d4691e90bcb82a46834d153de1fe9f550bfe29
Krzysztof Dorosz <cypreess@gmail.com>
2013-06-11 09:18:31 +0200

    Adding example validator

--- example/example/foo/forms.py
+++ example/example/foo/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm, HiddenInput
 from example.foo.models import Foo
+from example.foo.validators import max_foos_validator
+
 
 class FooForm(ModelForm):
     class Meta:
@@ -7,3 +9,8 @@ class FooForm(ModelForm):
         widgets = {
             'user' : HiddenInput,
         }
+
+    def clean(self):
+        cleaned_data = super(FooForm, self).clean()
+        max_foos_validator(cleaned_data['user'], add=1)
+        return cleaned_data
---
+++ example/example/foo/validators.py
@@ -0,0 +1,12 @@
+from example.foo.models import Foo
+from plans.validators import ModelCountValidator
+
+
+class MaxFoosValidator(ModelCountValidator):
+    code = 'MAX_FOO_COUNT'
+    model = Foo
+
+    def get_queryset(self, user):
+        return super(MaxFoosValidator, self).get_queryset(user).filter(user=user)
+
+max_foos_validator = MaxFoosValidator()
