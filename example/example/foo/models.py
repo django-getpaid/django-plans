eef4e2212bbedc2d73ff9fdaf8a28d685618b692
Krzysztof Dorosz <cypreess@gmail.com>
2013-06-11 08:02:15 +0200

    Nicer default value

--- example/example/foo/models.py
+++ example/example/foo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Foo(models.Model):
     user = models.ForeignKey('auth.User')
-    name = models.CharField(max_length=100, default="Default Foo name")
+    name = models.CharField(max_length=100, default="A new foo")
 
     def __unicode__(self):
         return self.name
