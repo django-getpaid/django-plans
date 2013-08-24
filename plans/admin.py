7e7526d25b29a71e95acc1063f02117db3e90d82
Krzysztof Dorosz <cypreess@gmail.com>
2013-08-24 18:31:11 +0200

    Adding more filter options

--- plans/admin.py
+++ plans/admin.py
@@ -1,10 +1,13 @@
 from copy import deepcopy
+
 from django.contrib import admin
 from django.core import urlresolvers
-from models import UserPlan, Plan, PlanQuota, Quota, PlanPricing, Pricing, Order, BillingInfo
 from ordered_model.admin import OrderedModelAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from models import UserPlan, Plan, PlanQuota, Quota, PlanPricing, Pricing, Order, BillingInfo
 from plans.models import Invoice
-from django.utils.translation import ugettext_lazy as _, pgettext_lazy
+
 
 class UserLinkMixin(object):
     def user_link(self, obj):
@@ -14,15 +17,17 @@ class UserLinkMixin(object):
     user_link.short_description = 'User'
     user_link.allow_tags = True
 
+
 class PlanQuotaInline(admin.TabularInline):
     model = PlanQuota
+
 
 class PlanPricingInline(admin.TabularInline):
     model = PlanPricing
 
-class QuotaAdmin(OrderedModelAdmin):
-    list_display = ('codename', 'name', 'description', 'unit', 'is_boolean',  'move_up_down_links', )
 
+class QuotaAdmin(OrderedModelAdmin):
+    list_display = ('codename', 'name', 'description', 'unit', 'is_boolean', 'move_up_down_links', )
 
 
 def copy_plan(modeladmin, request, queryset):
@@ -30,7 +35,7 @@ def copy_plan(modeladmin, request, queryset):
     Admin command for duplicating plans preserving quotas and pricings.
     """
     for plan in queryset:
-        plan_copy =  deepcopy(plan)
+        plan_copy = deepcopy(plan)
         plan_copy.id = None
         plan_copy.available = False
         plan_copy.default = False
@@ -46,12 +51,15 @@ def copy_plan(modeladmin, request, queryset):
             quota.id = None
             quota.plan = plan_copy
             quota.save(force_insert=True)
-copy_plan.short_description = _("Make plan copy")
+
+
+copy_plan.short_description = _('Make plan copy')
+
 
 class PlanAdmin(OrderedModelAdmin):
     search_fields = ('name', 'customized__username', 'customized__email', )
     list_filter = ( 'available',  )
-    list_display = ('name',   'description', 'customized', 'default', 'available', 'created', 'move_up_down_links')
+    list_display = ('name', 'description', 'customized', 'default', 'available', 'created', 'move_up_down_links')
     inlines = (PlanPricingInline, PlanQuotaInline)
     list_select_related = True
     raw_id_fields = ('customized',)
@@ -72,36 +80,46 @@ class BillingInfoAdmin(UserLinkMixin, admin.ModelAdmin):
 def make_order_completed(modeladmin, request, queryset):
     for order in queryset:
         order.complete_order()
-make_order_completed.short_description = _("Make selected orders completed")
+
+
+make_order_completed.short_description = _('Make selected orders completed')
+
 
 def make_order_invoice(modeladmin, request, queryset):
     for order in queryset:
         if Invoice.objects.filter(type=Invoice.INVOICE_TYPES['INVOICE'], order=order).count() == 0:
             Invoice.create(order, Invoice.INVOICE_TYPES['INVOICE'])
-make_order_invoice.short_description = _("Make invoices for orders")
+
+
+make_order_invoice.short_description = _('Make invoices for orders')
 
 
 class InvoiceInline(admin.TabularInline):
     model = Invoice
     extra = 0
 
+
 class OrderAdmin(admin.ModelAdmin):
-    list_filter = ('status', "plan")
+    list_filter = ('status', 'created', 'completed', 'plan__name', 'pricing')
     raw_id_fields = ('user',)
     search_fields = ('id', 'user__username', 'user__email')
-    list_display = ("id", "name", "created", "user", "status", "completed", "tax", "amount", "currency", "plan", "pricing")
+    list_display = (
+    'id', 'name', 'created', 'user', 'status', 'completed', 'tax', 'amount', 'currency', 'plan', 'pricing')
     actions = [make_order_completed, make_order_invoice]
     inlines = (InvoiceInline, )
+
     def queryset(self, request):
         return super(OrderAdmin, self).queryset(request).select_related('plan', 'pricing', 'user')
 
 
 class InvoiceAdmin(admin.ModelAdmin):
-    search_fields = ('full_number',  'buyer_tax_number', 'user__username', 'user__email')
-    list_filter = ('type', )
-    list_display = ('full_number', "issued", "total_net", "currency", 'user', "tax", "buyer_name", "buyer_city", "buyer_tax_number")
+    search_fields = ('full_number', 'buyer_tax_number', 'user__username', 'user__email')
+    list_filter = ('type', 'issued')
+    list_display = (
+    'full_number', 'issued', 'total_net', 'currency', 'user', 'tax', 'buyer_name', 'buyer_city', 'buyer_tax_number')
     list_select_related = True
     raw_id_fields = ('user', 'order')
+
 
 class UserPlanAdmin(UserLinkMixin, admin.ModelAdmin):
     list_filter = ('active', 'expire', 'plan__name')
@@ -111,7 +129,6 @@ class UserPlanAdmin(UserLinkMixin, admin.ModelAdmin):
     readonly_fields = ['user_link', ]
     fields = ('user_link', 'plan', 'expire', 'active' )
     raw_id_fields = ['plan', ]
-
 
 
 admin.site.register(Quota, QuotaAdmin)
