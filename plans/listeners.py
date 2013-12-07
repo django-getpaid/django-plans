28858ff06a58afe8e0b2bbfd81fe47d17a49c34e
Pierre Dulac <dulacpier@gmail.com>
2013-12-07 14:20:13 +0100

    Fix model in listener to use get_model instead

--- plans/listeners.py
+++ plans/listeners.py
@@ -41,7 +41,7 @@ def set_default_user_plan(sender, instance, created, **kwargs):
     if created:
         default_plan = get_model('plans', 'Plan').get_default_plan()
         if default_plan is not None:
-            UserPlan.objects.create(user=instance, plan=default_plan, active=False, expire=None)
+            get_model('plans', 'UserPlan').objects.create(user=instance, plan=default_plan, active=False, expire=None)
 
 
 # Hook to django-registration to initialize plan automatically after user has confirm account
