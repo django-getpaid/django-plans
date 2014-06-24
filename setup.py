5cbffb5433c34bf30dbb90b9067316b6f65e8d10
Andres Riancho <andres.riancho@gmail.com>
2014-06-24 12:28:12 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='django-plans',
-    version='0.7alpha',
+    version='0.8',
     description='Pluggable django app for managing pricing plans with quotas and accounts expiration',
     long_description=long_description,
     author='Krzysztof Dorosz',
