9dbad7ebbfc094ca3a244c6296f8910b776d227f
George Silva <george@consultoriasigma.com.br>
2015-01-22 10:07:02 -0200

    adding AppConfig for plans. backwards incompatible change.

--- plans/apps.py
+++ plans/apps.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 class PlansConfig(AppConfig):
 
+    name = "plans"
+    label = "plans"
+    verbose_name = "Django Plans"
+
     def ready(self):
 
         import plans.listeners
