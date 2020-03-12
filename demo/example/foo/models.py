2f7a903f1a6e975894dadd9ed7c89509bfc7ea6c
Petr Dlouhý <petr.dlouhy@email.cz>
2020-03-12 18:16:02 +0100

    Django 3.0 compatibility

--- demo/example/foo/models.py
+++ demo/example/foo/models.py
@@ -1,11 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
-from django.utils.encoding import python_2_unicode_compatible
 
-
-@python_2_unicode_compatible
 class Foo(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, default="A new foo")
