ac8d0b64793219b1b5ba16222bfde9aa32348fa3
Petr Dlouhý <petr.dlouhy@email.cz>
2023-05-16 16:15:21 +0200

    add possibility to set provider when running autorenew_accounts management command

--- plans/management/commands/autorenew_accounts.py
+++ plans/management/commands/autorenew_accounts.py
@@ -6,9 +6,18 @@ from plans import tasks
 class Command(BaseCommand):
     help = 'Autorenew accounts and with recurring payments'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--providers',
+            nargs='+',
+            dest='providers',
+            help='Renew only accounts with this providers',
+        )
+
     def handle(self, *args, **options):  # pragma: no cover
+        providers = options.get('providers')
         self.stdout.write("Starting renewal")
-        renewed_accounts = tasks.autorenew_account()
+        renewed_accounts = tasks.autorenew_account(providers)
         if renewed_accounts:
             self.stdout.write("Accounts submitted to renewal:")
             for a in renewed_accounts:
