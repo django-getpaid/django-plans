eec7cdd01315f5f6b89452ee71204d5798fa19a5
Petr Dlouhý <petr.dlouhy@email.cz>
2023-05-16 16:14:36 +0200

    make better output when running autorenew_accounts management command

--- plans/management/commands/autorenew_accounts.py
+++ plans/management/commands/autorenew_accounts.py
@@ -10,6 +10,8 @@ class Command(BaseCommand):
         self.stdout.write("Starting renewal")
         renewed_accounts = tasks.autorenew_account()
         if renewed_accounts:
-            self.stdout.write("Accounts autorenewed: " + ", ".join(str(s) for s in renewed_accounts))
+            self.stdout.write("Accounts submitted to renewal:")
+            for a in renewed_accounts:
+                self.stdout.write(f'\t{a.userplan.recurring.payment_provider}\t{a.email}\t{a}')
         else:
             self.stdout.write("No accounts autorenewed")
