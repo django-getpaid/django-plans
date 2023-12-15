564e16d862a7a9b32f565dad9d30a52627bc9c0d
Victor Hugo dos Santos <listas.vhs@gmail.com>
2023-12-15 13:28:28 +0100

    Fixed wrong db path

--- demo/example/settings.py
+++ demo/example/settings.py
@@ -25,7 +25,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
