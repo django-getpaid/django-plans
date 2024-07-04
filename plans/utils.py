7da64569ed4de56af09f132acf86bd9eb65d2f17
Milan Slesarik <milslesarik@gmail.com>
2024-07-05 01:20:15 +0200

    Refactor tax calculation logic in AbstractOrder.recalculate

--- plans/utils.py
+++ plans/utils.py
@@ -1,5 +1,9 @@
+from decimal import Decimal
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+
+from plans.importer import import_name
 
 
 def get_client_ip(request):
@@ -43,3 +47,58 @@ def country_code_transform(country_code):
         "GR": "EL",
     }
     return transform_dict.get(country_code, country_code)
+
+
+def calculate_tax_rate(tax_number, country_code, request=None):
+    taxation_policy = getattr(settings, "PLANS_TAXATION_POLICY", None)
+    if not taxation_policy:
+        raise ImproperlyConfigured("PLANS_TAXATION_POLICY is not set")
+    taxation_policy = import_name(taxation_policy)
+    tax, request_successful = taxation_policy.get_tax_rate(
+        tax_number, country_code, request
+    )
+    if request_successful and request:
+        TaxCacheService.cache_tax_rate(request, tax, tax_number, country_code)
+    return Decimal(tax) if tax is not None else None, request_successful
+
+
+def get_tax_rate(country_code, tax_number, request=None):
+    """Get tax rate for given country and tax number
+    1. Try to get tax rate from cache
+    2. If not in cache, calculate it (and possibly cache it)
+
+    Returns tax rate and if the request was successful (False means default tax rate was used)
+    """
+    if request:
+        try:
+            tax_from_cache = TaxCacheService.get_tax_rate(
+                request, tax_number, country_code
+            )
+            return tax_from_cache, True
+        except KeyError:
+            pass
+
+    tax, request_successful = calculate_tax_rate(tax_number, country_code, request)
+    return tax, request_successful
+
+
+class TaxCacheService:
+    @classmethod
+    def get_cache_key(cls, tax_number, country):
+        return "tax_%s_%s" % (tax_number, country)
+
+    @classmethod
+    def cache_tax_rate(cls, request, tax, tax_number, country):
+        request.session[cls.get_cache_key(tax_number, country)] = str(tax)
+
+    @classmethod
+    def get_tax_rate(cls, request, tax_number, country):
+        key = cls.get_cache_key(tax_number, country)
+        if key not in request.session:
+            raise KeyError(
+                f"Tax rate for {tax_number} and {country} not found in cache"
+            )
+        raw = request.session[key]
+        if raw == "None":
+            return None
+        return Decimal(raw)
