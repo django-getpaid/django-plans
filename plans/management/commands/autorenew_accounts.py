f0f552674381e08b36a022237a70c266b9040a86
Petr Dlouhý <petr.dlouhy@email.cz>
2025-06-05 12:20:25 +0200

    catch exceptions during autorenew (--catch-exceptions option)

--- plans/management/commands/autorenew_accounts.py
+++ plans/management/commands/autorenew_accounts.py
@@ -19,13 +19,22 @@ class Command(BaseCommand):
             dest="throttle",
             help="Throttle seconds between renewals",
         )
+        parser.add_argument(
+            "--catch-exceptions",
+            action="store_true",
+            dest="catch_exceptions",
+            help="Catch exceptions during renewal and log them",
+        )
 
     def handle(self, *args, **options):  # pragma: no cover
         providers = options.get("providers")
         self.stdout.write("Starting renewal")
         throttle_seconds = options.get("throttle")
+        catch_exceptions = options.get("catch_exceptions")
         renewed_accounts = tasks.autorenew_account(
-            providers, throttle_seconds=throttle_seconds
+            providers,
+            throttle_seconds=throttle_seconds,
+            catch_exceptions=catch_exceptions,
         )
         if renewed_accounts:
             self.stdout.write("Accounts submitted to renewal:")
